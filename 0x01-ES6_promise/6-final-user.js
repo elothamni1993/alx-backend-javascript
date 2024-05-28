2d382990e2e756116906e121b519d3424e4b1782
the0zero0 <cryptologia6@gmail.com>
2024-05-28 20:38:28 +0100

    js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
+}
